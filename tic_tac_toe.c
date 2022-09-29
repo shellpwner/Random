850109c089221c548535d817da8bc11ccb55e0c7
shellpwner <104843500+shellpwner@users.noreply.github.com>
2022-09-29 07:50:24 +0530

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -5,47 +5,42 @@ void dispGrid(int** grid){
 	system("clear");
 	for(int i=0;i<3;i++){									//			
 		for(int j=0;j<3;j++){								//		
-			if(grid[i][j] == 5){                    		//
-				printf("   |");								//Translating integer grid to spaces and XOs
+			if(grid[i][j] == 5){                    				//
+				printf("   |");							//Translating integer grid to spaces and XOs
 			}else if(grid[i][j] == 1){						//	
-				printf(" X |");								//		
-			}else{											//			
-				printf(" O |");								//			
+				printf(" X |");							//		
+			}else{									//			
+				printf(" O |");							//			
 			}
 		}printf("\n");
 	}
-	// for(int i=0;i<3;i++){
-	// 	for(int j=0;j<3;j++){			//	  debugging
-	// 		printf("%d",grid[i][j]);      //    debugging 
-	// 	}printf("\n");					//debugging
-	// }								//debugging
 }
 
 void p1(int** grid){
-	int x,y;															//
-	printf("Player 1 (X): ");											// Player 1's board placement	
-	scanf("%d,%d",&x,&y);												// 
+	int x,y;										//
+	printf("Player 1 (X): ");								// Player 1's board placement	
+	scanf("%d,%d",&x,&y);									// 
 	while(grid[x][y] != 5){									//verification if the chosen spot is vacant or not
-		printf("That cell has already been occupied, pick again: ");	//
-		scanf("%d,%d",&x,&y);											//loop until the entered point is vacant	
-	}																	//
-	grid[x][y] = 1;														//set the board
+		printf("That cell has already been occupied, pick again: ");	                //
+		scanf("%d,%d",&x,&y);								//loop until the entered point is vacant	
+	}											//
+	grid[x][y] = 1;										//set the board
 	
 }
 
 void p2(int** grid){
-	int x,y;															//
-	printf("Player 2 (O): ");											// Player 2's board placement	
-	scanf("%d,%d",&x,&y);												// 
+	int x,y;										//
+	printf("Player 2 (O): ");								// Player 2's board placement	
+	scanf("%d,%d",&x,&y);									// 
 	while(grid[x][y] != 5){									//verification if the chosen spot is vacant or not
 		printf("That cell has already been occupied, pick again: ");	//
-		scanf("%d,%d",&x,&y);											//loop until the entered point is vacant	
-	}																	//
-	grid[x][y] = 0;														//set the board
+		scanf("%d,%d",&x,&y);								//loop until the entered point is vacant	
+	}											//
+	grid[x][y] = 0;										//set the board
 	
 }
 
-int wld_verify(int** grid){		//this function will return 1 if x won, 0 is O won, -1 if draw and 5 if no result
+int wld_verify(int** grid){						//this function will return 1 if x won, 0 is O won, -1 if draw and 5 if no result
 	int sumr(int i){
 		return grid[i][0] + grid[i][1] + grid[i][2];
 	}
@@ -68,15 +63,11 @@ int wld_verify(int** grid){		//this function will return 1 if x won, 0 is O won,
 		}
 	}
 	for(int i=0;i<9;i++){
-		if(nums[i] == 5){       // chekcing if there are any empty places in the grid
-			flag = 1;			// flag becomes 1 if there are empty spots. Else remains 0 (default)
+		if(nums[i] == 5){   					    // chekcing if there are any empty places in the grid
+			flag = 1;					// flag becomes 1 if there are empty spots. Else remains 0 (default)
 			break;
 		}
 	}
-	// for(int i=0;i<8;i++){					//debugging
-	// 	printf("%d  ",sums[i]);				  //  debugging 
-	// }printf("\n");							//debugging 
-	// printf("Flag: %d\n",flag);
 	// if flag is 0 and the sums array doesn't contain either 3 or 0; then its a draw
 	int sums_check(int* sums){ //returns -1 if no win, 1 if 3 is in array, 0 if 0 is in array
 		int ret;
@@ -85,7 +76,6 @@ int wld_verify(int** grid){		//this function will return 1 if x won, 0 is O won,
 				ret = 1;      // X wins
 				break;
 			}else if(sums[i] == 0){
-				//printf("i at which sums is 0: %d\n",i);     /debugging
 				ret = 0;      //O wins
 				break;
 			}else{
@@ -109,14 +99,14 @@ int wld_verify(int** grid){		//this function will return 1 if x won, 0 is O won,
 
 int main(){
 	int** grid;
-	grid = (int**)malloc(3*sizeof(int*));				//
-	for(int i=0;i<3;i++){                               //creating the grid
-		grid[i] = (int*)malloc(3*sizeof(int));			//
+	grid = (int**)malloc(3*sizeof(int*));					
+	for(int i=0;i<3;i++){                             			  //creating the grid
+		grid[i] = (int*)malloc(3*sizeof(int));				
 	}	
 
-	for(int i=0;i<3;i++){								//
-		for(int j=0;j<3;j++){							//filling thr grid with emptie cells. (5-empty, 1-X, 0-O)
-			grid[i][j] = 5;								//
+	for(int i=0;i<3;i++){								
+		for(int j=0;j<3;j++){						//filling thr grid with emptie cells. (5-empty, 1-X, 0-O)
+			grid[i][j] = 5;								
 		}
 	}
 	while(1>0){
@@ -152,3 +142,10 @@ int main(){
 	}
 	return 0;
 }
+/*the coordinates of each cell in the grid are as follows: (to be used while running the program)
+
+0,0 | 0,1 | 0,2
+1,0 | 1,1 | 1,2
+2,0 | 2,1 | 2,2
+
+*/
